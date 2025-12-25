df8b2506a15065f49b9b457d3568d89f49b8293d
Lingadurai <lingaduraip@codewave.com>
2025-12-26 01:39:58 +0530

    ci: sign tauri msi

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,8 @@ function App() {
       {/* Your existing app content */}
       <h1>hello Lingadurai</h1>
 
-      <h2>Update 0.1.2</h2>
+      <h2>Update 0.1.3</h2>
+      <h3>update done by github action </h3>
 
       {updateInfo && <h1>{updateInfo.toString()}</h1>}
       {updateAvailable && (
